548b0e3fb1cb0c6cc85780ad91735ebbc24bbb66
Kailash Khadka <k.khadka343@gmail.com>
2025-06-10 01:10:53 +0000

    Updated to accomodate localization service

--- scripts/start_warehouse_automation.bash
+++ scripts/start_warehouse_automation.bash
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+echo "WELCOME TO WAREHOUSE AUTOMATION SHELL"
+# ==== ENVIRONMENT ====
+ENV=${1:-sim} #Default: sim
+
 # ==== Functions ====
 killgazeboprocesses() {
     echo "Searching for Gazebo processes..."
@@ -14,12 +19,14 @@ rm -rf ~/ros2_ws/src/warehouse_automation/logs
 mkdir -p $LOG_DIR
 
 # ==== Install Dependencies ====
+echo
 echo "[INFO] Updating package list..."
 sudo apt update > $LOG_DIR/install_dependencies.log 2>&1
 sudo apt install -y --no-install-recommends moreutils >> $LOG_DIR/install_dependencies.log 2>&1
 echo "[INFO] Dependencies installed successfully"
 
 # ==== Build projects ====
+echo
 echo "Building projects..."
 cd ~/ros2_ws && colcon build  > >(ts '[%Y%m%d%H%M%S]' > $LOG_DIR/ros2_ws_build.log) 2>&1
 source ~/ros2_ws/install/setup.bash
@@ -27,6 +34,7 @@ source ~/ros2_ws/install/setup.bash
 # ==== Start Warehouse Automation ====
 
 # ==== Find the environment: SIM or REAL ====
+echo
 source /opt/ros/${ROS_DISTRO}/setup.bash
 if [[ "$ENV" == "real" ]]; then
     echo "Welcome to the WAREHOUSE LAB!"
@@ -63,13 +71,15 @@ else
 fi
 
 # ==== LOCALIZATION SERVER ====
+echo
 echo "Starting localization server"
 MAX_ATTEMPTS=3
 ATTEMPT=1
 
 while [ $ATTEMPT -le $MAX_ATTEMPTS ]; do
-    cd ~/ros2_ws
-    ros2 launch localization_server localization.launch.py map_file:=warehouse_map_keepout_${ENV}.yaml > >(ts '[%Y%m%d%H%M%S]' > "$LOG_DIR/localization_${ENV}.log") 2>&1 &
+    cd ~/ros2_ws && source install/setup.bash
+    ros2 launch localization_server localization.launch.py map_file:=warehouse_map_keepout_${ENV}.yaml \
+        > >(ts '[%Y%m%d%H%M%S]' > "$LOG_DIR/localization_${ENV}.log") 2>&1 &
     echo "Wait while localization service is loading..."
     sleep 25
 
@@ -90,32 +100,43 @@ if [ $ATTEMPT -gt $MAX_ATTEMPTS ]; then
     exit 1
 fi
 
-sleep 5
-
 # ==== PATH PLANNING SERVER ====
+echo
 echo "Starting pathplanning service"
-ros2 launch path_planner_server pathplanner.launch.py use_sim_time:=${USE_SIM_TIME} > >(ts '[%Y%m%d%H%M%S]' > "$LOG_DIR/pathplanner_${ENV}.log") 2>&1 &
+cd ~/ros2_ws && source install/setup.bash
+ros2 launch path_planner_server pathplanner.launch.py use_sim_time:=${USE_SIM_TIME} \
+  > >(grep -viE "drop|rviz" | ts '[%Y%m%d%H%M%S]' > "$LOG_DIR/pathplanner_${ENV}.log") 2>&1 &
+
+echo "Wait while pathplanner service is loading..."
 sleep 25
+echo "Pathplanner is running successfully. Proceeding..."
+
+# ==== Start Global Localization Action Server ====
+echo
+echo "Starting global localization service server"
+
+source ~/ros2_ws/install/setup.bash
+ros2 run warehouse_automation_pkg global_localization_node --ros-args -p env:=${ENV} \
+    > >(ts '[%Y%m%d%H%M%S]' > "$LOG_DIR/glocalization_service_${ENV}.log") 2>&1 &
 
 # ==== Clone webpage repository ====
 echo
+echo "Starting webpage"
 cd ~/webpage_ws
 # git pull
 python3 -m http.server 7000 > >(ts '[%Y%m%d%H%M%S]' > "$LOG_DIR/webpage_${ENV}.log") 2>&1 &
-echo "Started webpage address:"
+
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 echo "https://${instance_id}.robotigniteacademy.com/${SLOT_PREFIX}/webpage/"
 echo "Rosbridge address:"
 echo "wss://${instance_id}.robotigniteacademy.com/${SLOT_PREFIX}/rosbridge/"
 
+# ==== Run rosbridge server ====
 echo
+echo "Launch rosbridge server"
 cd ~/ros2_ws
-ros2 launch rosbridge_server rosbridge_websocket_launch.xml > >(ts '[%Y%m%d%H%M%S]' >> "$LOG_DIR/webpage_${ENV}.log") 2>&1 &
+ros2 launch rosbridge_server rosbridge_websocket_launch.xml \
+    > >(ts '[%Y%m%d%H%M%S]' >> "$LOG_DIR/webpage_${ENV}.log") 2>&1 &
 sleep 5
 
 echo "[!] All processes launched. Check logs in $LOG_DIR"
-
-# cd ros2_ws/src/warehouse_automation
-# ./scripts/start_warehouse_automation.bash
-# killwarehouseautomationprocesses
-
